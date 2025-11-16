2c117bc054d91296a52f848d2e79bc9a3efd0988
Dragos-Stefan Necula <dragosstefannecula@gmail.com>
2025-11-16 18:10:48 +0000

    Added a environment variable to enable development mode when using "npm run start"

--- src/main.js
+++ src/main.js
@@ -40,7 +40,7 @@ function createWindow() {
     }
   });
 
-  const isDev = false;
+  const isDev = process.env.NODE_ENV === 'development';
   const startURL = isDev 
   ? 'http://localhost:5173' // Vite dev server
   : `file:/${__dirname}/dist/renderer/index.html`;
