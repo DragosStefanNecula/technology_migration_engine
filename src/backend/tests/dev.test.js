4ca75332d833341ace7001ee9926f50b4e47d636
Dragos-Stefan Necula <dragosstefannecula@gmail.com>
2025-11-17 12:17:02 +0000

    Created a dev environment for live debugging

---
+++ src/backend/tests/dev.test.js
@@ -0,0 +1,7 @@
+import test from 'node:test';
+import assert from 'node:assert';
+
+import { parse } from '../components/parser.js';
+// test('parse simple JavaScript snippet', () => {
+//   console.log(parse("sub greet :Path('greet') :Args(1)"))
+// });
