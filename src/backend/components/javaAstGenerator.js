5f5c00c288c2eb956563e5d08937bf755dff2c98
Dragos-Stefan Necula <dragosstefannecula@gmail.com>
2026-01-18 11:18:04 +0000

    removed debugging console logs

--- src/backend/components/javaAstGenerator.js
+++ src/backend/components/javaAstGenerator.js
@@ -6,11 +6,9 @@ function gen(node){
 
     if(node.type === "source_file"){
         let body = [];
-        console.log(node.children);
         node.children.forEach(child => {
             body.push(gen(child));
         });
-        console.log(body)
 
         return {
             type: 'SourceFile',
