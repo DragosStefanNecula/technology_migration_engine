b41939ee800d07fbd26a9d6c6ab8815153650ca0
Dragos-Stefan Necula <dragosstefannecula@gmail.com>
2026-02-03 10:10:57 +0000

    Updated and cleaned up Npm Run Console to also run the other tests

--- src/backend/tests/dev.test.js
+++ src/backend/tests/dev.test.js
@@ -1,15 +1,14 @@
 import test from 'node:test';
-import assert from 'node:assert';
 
 import { genJavaAstDebug, parseDebugOutput } from './testingFunctions.js';
 import { handlePerl } from '../components/migrationLogic.js';
-test('parse simple JavaScript snippet', () => {
-const input = `
-  $a = $a + $b;
-`;
+
+test('Dev', () => {
+  const input = `
+    $a = $a > $b;
+  `;
     
   console.log(parseDebugOutput(input));
-  // console.log(genJavaAstDebug(input).body[0].params);
-  // genJavaAstDebug(input);
+  console.log(genJavaAstDebug(input));
   console.log(handlePerl(input));
 });
