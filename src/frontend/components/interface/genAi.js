d7d7108fee6f612009728650d33f5907ce22d31f
Dragos-Stefan Necula <dragosstefannecula@gmail.com>
2026-03-24 16:50:36 +0000

    docs(frontend): Added comment about adding error handling

--- src/frontend/components/interface/genAi.js
+++ src/frontend/components/interface/genAi.js
@@ -1,6 +1,7 @@
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export async function genAi(){
+    //TODO: Error handling too
     await delay(2000);
     return "text";
 }
