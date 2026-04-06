73e239478f3a52bbba5ace6f9ee796af2e20f2c3
Dragos-Stefan Necula <dragosstefannecula@gmail.com>
2026-04-06 22:24:58 +0300

    format(frontend): Reformatting SelectioButtons.jsx

--- src/frontend/components/interface/SelectionButtons.jsx
+++ src/frontend/components/interface/SelectionButtons.jsx
@@ -2,19 +2,27 @@ import React from 'react';
 import Button from '../base/Button';
 
 export const SelectionButtons = ({ setCurrentView, currentView, onConfirm, firstPass, secondPass }) => {
-  return (
-    <div style={{height: "auto", width: "15%" }}>
-        <div style={{height: "100%", width: "auto", display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-        }}>
-            <Button style={{marginBlock:"2px"}} selected={currentView === "1stPass"} onClick={() => setCurrentView("1stPass")}>1st Pass</Button>
-            <Button style={{marginBlock:"2px"}} clickable={firstPass} selected={currentView === "2ndPass"} onClick={() => setCurrentView("2ndPass")}>2nd Pass</Button>
-            <Button style={{marginBlock:"2px"}} clickable={secondPass} onClick={onConfirm} variant={"green"}>Confirm</Button>
-        </div> 
-    </div>
-  );
+    return (
+        <div style={{ height: "auto", width: "15%" }}>
+            <div style={{
+                height: "100%", width: "auto", display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+            }}>
+                <Button style={{ marginBlock: "2px" }}
+                    selected={currentView === "1stPass"}
+                    onClick={() => setCurrentView("1stPass")}>1st Pass</Button>
+                <Button style={{ marginBlock: "2px" }}
+                    clickable={firstPass}
+                    selected={currentView === "2ndPass"}
+                    onClick={() => setCurrentView("2ndPass")}>2nd Pass</Button>
+                <Button style={{ marginBlock: "2px" }} variant={"green"}
+                    clickable={secondPass}
+                    onClick={onConfirm}>Confirm</Button>
+            </div>
+        </div>
+    );
 };
 
 export default SelectionButtons; 
